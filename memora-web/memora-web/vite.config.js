f57e9a0d9b8d61f2daa4e442b6f6989f113fd3d5
Luiz Yan <luizyan68@gmail.com>
2026-05-23 08:44:37 -0300

    problemas de pastas resolvido

--- memora-web/memora-web/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Configuração para o Vite reconhecer a pasta do GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: '/memora-web/',
-})
